0c2163b4af8098fec76543dedf872dd6216a5236
mocatfrio <hafarafirdausi@gmail.com>
2023-10-05 14:57:14 +0700

    add hashing

--- Hashing/hashing_map1.cpp
+++ Hashing/hashing_map1.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 map<string, map<string, string> > dataAkun;
 
-// Hash Functiion
+// Hash Function
 int hashFunction(string password)
 {
-  // utak atik disini
+  // Assignment: Make a good hash function here
   return tolower(password[0]) - 97;
 }
 
@@ -32,6 +32,30 @@ void registerAkun()
   dataAkun[hash_key] = dataDetail;
 }
 
+void loginAkun()
+{
+  string username, password;
+  cout << "Masukkan Username: " << endl;
+  cin >> username;
+  cout << "Masukkan Password: " << endl;
+  cin >> password;
+
+  string hash_key = to_string(hashFunction(password));
+
+  cout << endl;
+  if (dataAkun.find(hash_key) != dataAkun.end())
+  {
+    cout << "Login berhasil!" << endl
+         << endl;
+    cout << "==== Account Details ====" << endl;
+    cout << "Username: " << dataAkun[hash_key]["username"] << endl;
+    cout << "Fullname: " << dataAkun[hash_key]["username"] << endl;
+  } else {
+    cout << "Login gagal!" << endl
+         << endl;
+  }
+}
+
 void printAkun()
 {
   cout << endl;
@@ -52,9 +76,10 @@ void menu()
     cout << endl;
     cout << "==== MENU ====" << endl;
     cout << "1. Register" << endl;
-    // cout << "2. Login" << endl;
-    cout << "2. Exit" << endl;
+    cout << "2. Login" << endl;
+    cout << "3. Exit" << endl;
 
+    cout << endl;
     cout << "Pilih Menu: " << endl;
     cin >> input_user;
 
@@ -64,8 +89,16 @@ void menu()
     }
     else if (input_user == 2)
     {
+      loginAkun();
+    }
+    else if (input_user == 3)
+    {
       printAkun();
       break;
+    }
+    else
+    {
+      cout << "Invalid!" << endl;
     }
   }
 }
