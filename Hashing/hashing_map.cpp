80bc7d2cfdcd8650c9ed8e126455a6bf34452ef0
mocatfrio <hafarafirdausi@gmail.com>
2023-10-05 14:58:48 +0700

    add hashing

--- Hashing/hashing_map.cpp
+++ Hashing/hashing_map.cpp
@@ -1,27 +1,72 @@
 #include <iostream>
 #include <map>
+
 using namespace std;
 
-// Sebuah sistem login sederhana
+map<string, map<string, string> > dataAkun;
 
-map<string, string> dataAkun;
-
-// Hash
+// Hash Function
 int hashFunction(string password)
 {
-  // Tugas: utak atik bagian sini!
+  // Assignment: Make a good hash function here
   return tolower(password[0]) - 97;
 }
 
-void printAll()
+void registerAkun()
 {
-  for (auto i = dataAkun.begin(); i != dataAkun.end(); i++)
+  string username, password, fullname;
+  cout << "Masukkan Username: " << endl;
+  cin >> username;
+  cout << "Masukkan Password: " << endl;
+  cin >> password;
+  cout << "Masukkan Nama Lengkap: " << endl;
+  cin >> fullname;
+
+  string hash_key = to_string(hashFunction(password));
+
+  map<string, string> dataDetail;
+  dataDetail["username"] = username;
+  dataDetail["fullname"] = fullname;
+
+  // masukkan ke map
+  dataAkun[hash_key] = dataDetail;
+}
+
+void loginAkun()
+{
+  string username, password;
+  cout << "Masukkan Username: " << endl;
+  cin >> username;
+  cout << "Masukkan Password: " << endl;
+  cin >> password;
+
+  string hash_key = to_string(hashFunction(password));
+
+  cout << endl;
+  if (dataAkun.find(hash_key) != dataAkun.end())
   {
-    cout << "Key: " << i->first << " \t\t\t"
-         << "Value: " << i->second << endl;
+    cout << "Login berhasil!" << endl
+         << endl;
+    cout << "==== Account Details ====" << endl;
+    cout << "Username: " << dataAkun[hash_key]["username"] << endl;
+    cout << "Fullname: " << dataAkun[hash_key]["username"] << endl;
+  } else {
+    cout << "Login gagal!" << endl
+         << endl;
   }
 }
 
+void printAkun()
+{
+  cout << endl;
+  for (auto i = dataAkun.begin(); i != dataAkun.end(); i++)
+  {
+    cout << "Key: " << i->first << "\t"
+         << " Username: " << i->second["username"] << " Fullname: " << i->second["fullname"] << endl;
+  }
+}
+
+// Menu
 void menu()
 {
   while (true)
@@ -29,47 +74,26 @@ void menu()
     int input_user;
 
     cout << endl;
-    cout << "MENU" << endl;
+    cout << "==== MENU ====" << endl;
     cout << "1. Register" << endl;
-    // cout << "2. Login" << endl;
-    cout << "3. Exit" << endl
-         << endl;
+    cout << "2. Login" << endl;
+    cout << "3. Exit" << endl;
 
-    cout << "Pilih: " << endl;
-    cin >> input_user;
     cout << endl;
+    cout << "Pilih Menu: " << endl;
+    cin >> input_user;
 
     if (input_user == 1)
     {
-      string username, password;
-
-      cout << "Masukkan Username: " << endl;
-      cin >> username;
-      cout << "Masukkan Password: " << endl;
-      cin >> password;
-
-      string index = to_string(hashFunction(password));
-      dataAkun[index] = username;
-
-      // if (dataAkun[index].username.empty() && dataAkun[index].password.empty())
-      // {
-      //   dataAkun[index].username = username;
-      //   dataAkun[index].password = password;
-      // }
-      // else
-      // {
-      //   cout << "Akun sudah ada!" << endl;
-      // }
+      registerAkun();
     }
     else if (input_user == 2)
     {
-      // Menu Login
-      cout << "Menu Login" << endl;
+      loginAkun();
     }
     else if (input_user == 3)
     {
-      printAll();
-
+      printAkun();
       break;
     }
     else
